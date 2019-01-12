89df87364e62c935b28a0f4731761c747741bdf3
Avery <cbra067@lasierra.edu>
2019-01-12 11:34:08 -0800

    more stuff

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,22 @@
-<<<<<<< HEAD
 var tables = require("../data/tableData");
 var waiting = require("../data/waitinglistData");
 
 module.exports = function(app){
   app.get("/api/tables", function(req, res) {
-=======
-var tables = require("../data/tableData.js");
-var waiting = require("../data/waitinglistData.js");
-app.get("/api/tables", function(req, res) {
->>>>>>> 6b09d5d99adfdcae4b64b3f4fabaa814c5ad357a
-    return res.json(tables);
+    return res.json(tables.tables);
   });
   app.get("/api/waiting", function(req, res) {
-    return res.json(waiting);
+    return res.json(waiting.waiting);
   });
   
 
 //for new reservations
-app.post("/api/reserve", function(req, res) {
-  
-  var newreservation = req.body;
-
-
-  newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newreservation);
-
-  reservations.push(newreservation);
-
-  res.json(newreservation);
+  app.post("/api/reserve", function(req, res) {
+    var newreservation = req.body;
+    newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newreservation);
+    reservations.push(newreservation);
+    res.json(newreservation);
 });
 }
 
