9c347b78a2fbbc79d17b75b1e9571728468fb682
Kfill <fillpotk@gmail.com>
2019-01-12 11:23:32 -0800

    updated html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,15 @@
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "home.html"));
+      });
+      
+      app.get("/reservations", function(req, res) {
+        res.sendFile(path.join(__dirname, "reserve.html"));
+      });
+      
+      app.get("/tables", function(req, res) {
+        res.sendFile(path.join(__dirname, "tables.html"));
+      });
+}
 
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-  app.get("/reservations", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-  
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-  });
-  
